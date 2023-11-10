2abb5d81e9df574a47686d7fd71ad18e4742ec7a
Cora Spruill <corradora30@gmail.com>
2023-11-10 11:46:55 -0500

    Ch9 hw complete

--- CH9-DTCombo/example/SpaceOrderTest.java
+++ CH9-DTCombo/example/SpaceOrderTest.java
@@ -12,7 +12,12 @@ public class SpaceOrderTest {
    public Object[][] getAcceptOrderData() {
       return new Object[][] {
          //                 TID, special, space,  e_rv, e_accept
-         {  "SpaceOrderTest T3",    true,     7,  true,     true},
+         {  "T1.1",    false,     8,  true,     false},
+           {  "T1.2",    false,     512,  true,     true},
+           {  "T1.3",    false,     2000,  true,     false},
+           {  "T1.4",    true,     8,  true,     true},
+           {  "T1.5",    true,     512,  true,     true},
+           {  "T1.6",    true,     2000,  true,     false},
       };
    }
 
