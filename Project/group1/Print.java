cb90a90fe09c27b543be8d14d84389138ef6a287
Cora Spruill <corradora30@gmail.com>
2023-11-10 11:08:40 -0500

    Initial Ch9 Homework Commit

--- Project/group1/Print.java
+++ Project/group1/Print.java
@@ -7,27 +7,23 @@ import static group1.PrintConstants.*;
  * Sizes: 4x6, 5x7, 8x10
  * Finish: Glossy or Matte
  * Processing time: 24 hours or 1 hour
- * Client should have the option to pick -same- or -different- sizes/finishes/processing times
-
+ * Client should have the option to pick -same- or -different- sizes/finishes/processing time
  * Standard values: Size = 4x6, Finish = glossy, processingTime = DAY
  * Standard value pricing: 1-50 prints = $0.14 each, 51-75 prints = $0.12, 76=100 prints = $0.10
  * 5 x 7 pricing: 1-50 prints = $0.34, 51-75 prints = $0.31, 76-100 prints = $0.28
  * 8 x 10 pricing: 1-50 prints = $0.68, 51-75 prints = $0.64, 76-100 prints = $0.60
- *
  * Matte finish pricing: $0.02 for each print 4x6, $0.03 for each print 5x7, $0.04 for each print 8x10
- *
  * if processingTime = HOUR, add $1.00 if <= 60 prints, $1.50 if > 60 prints && sameType == True
  * if discountCode == N56M2, subtract $2 only if numOfPrints = 100 and same values for size, finish, processingTime
- *
  * if different sizes, finishes, processingTimes chosen:
         * $0.19 for each print 4x6, $0.39 for each print 5x7, $0.79 for each print 8x10
         * If matte == True, add $0.04 each 4x6, $0.06 for each 5x7, $0.08 for each 8x10
         * If processingTime = HOUR, add $2.00 if <= 60 prints, $2.50 if > 60 prints
- * If total cost >= $35, client can obtain a 5% discount. Can NOT be combined w/ discountCode     */
+ * If total cost >= $35, client can obtain a 5% discount. Can NOT be combined w/ discountCode
+ * Customer notes: Do not consider different processing times, either all 1 hour or all 1 day.     */
 
 public class Print {
     public enum Time {DAY, HOUR}
-
     public static float cost(int four_by_six, int five_by_seven, int eight_by_ten,
                              int four_six_matte, int five_seven_matte, int eight_ten_matte,
                              Time processingTime, String discountCode) {
@@ -37,24 +33,21 @@ public class Print {
         boolean sameType = determineSameType(numOfPrints, four_by_six, five_by_seven, eight_by_ten, four_six_matte, five_seven_matte, eight_ten_matte);
         if (numOfPrints > 0 && numOfPrints <= 100) {
             cost = calculateBaseCost(four_by_six, five_by_seven, eight_by_ten, sameType, numOfPrints);
-
             // Add processing time cost
             cost += calculateProcessingTimeCost(numOfPrints, processingTime, sameType);
-
             // Add matte costs
             cost += calculateMatteCost(four_six_matte, five_seven_matte, eight_ten_matte, sameType);
-
             // Apply discount code
             if (discountCode.equals(DISCOUNT_CODE)) {
                 cost = applyDiscountCode(cost, numOfPrints, sameType);
             }
-
             // Discount if cost is >= 35 (can't be combined with discount code)
             if (cost >= 35.0f && !discountCode.equals(DISCOUNT_CODE)){
                 cost = cost * .95f;
                 cost = Math.round(cost * 100.0f) / 100.0f;
             }
         }
+
         return cost;
     }
 
